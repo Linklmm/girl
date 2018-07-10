4677427c2d3b8b9e81eb0c45dd52921f7888814c
origin <https://github.com/Linklmm/girl.git>
2018-07-10 15:10:22 +0800

    第一个spring boot代码

---
+++ src/main/java/com/spbtest/HelloController.java
@@ -0,0 +1,23 @@
+package com.spbtest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    private GirlProperties girlProperties;
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    String say(){
+
+        //return "hello spring boot";
+        return girlProperties.getCupSize();
+    }
+
+}
